219395af817ad9688a905faee648146a132f3d44
Anton200021243 <mortal0011@gmail.com>
2023-02-04 23:34:50 +0300

    Init
    second init

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuManager : MonoBehaviour
+{
+
+    private InputField name;
+    [SerializeField] Button startGame;
+    [SerializeField] Button exit;
+
+    private void Start()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
+    }
+
+}
